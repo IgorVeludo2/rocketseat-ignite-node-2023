0d6521a7be75379eb179dce031f81a34cad6c55f
IgorVeludo2 <velvet.guimaraes@gmail.com>
2023-06-02 17:25:30 -0300

    knex config

--- ignite-projeto2/src/database.ts
+++ ignite-projeto2/src/database.ts
@@ -1,8 +1,15 @@
-import { knex as knexConfig } from 'knex'
+import { knex as knexConfig, Knex } from 'knex'
 
-export const knex = knexConfig({
+export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './tmp/app.db',
+    filename: './db/app.db',
   },
-})
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+}
+
+export const knex = knexConfig(config)
